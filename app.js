19cf108615c1fcd5da0cc50ac4ad64ab288b0472
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2025-01-18 23:33:28 -0300

    Cria array para armazenar os nome e Implementa uma função para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Crie um array para armazenar os nomes
+let amigos = [];
+
+// Implementa uma função para agregar amigos
+function adicionarAmigo() {
+    // Capturar o valor do campo de entrada
+    let nomeAmigo = document.getElementById('amigo');
+
+    // Validar a entrada
+    if (nomeAmigo.value == '') {
+        // Se estiver vazio, exiba um alerta com a mensagem de erro
+        alert('Por favor, insira um nome.');
+    } else {
+        // Se o valor for válido, adicione-o ao array que armazena os nomes dos amigos
+        amigos.push(nomeAmigo.value);
+        // Limpar o campo de entrada
+        nomeAmigo.value = '';
+        atualizarLista();
+    }
+
+}
